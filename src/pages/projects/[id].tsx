d61741cd3fe320420a595ee352b64bcbd05b8d00
Marcelino Teixeira <marceometry@gmail.com>
2021-05-12 22:13:39 -0300

    add api calls

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -14,7 +14,7 @@ type ProjectProps = {
 export default function Project({ project }: ProjectProps) {
     return (
         <div>
-            {/* <Head>
+            <Head>
                 <title>Marcelino | {project.name}</title>
             </Head>
 
@@ -70,7 +70,7 @@ export default function Project({ project }: ProjectProps) {
                     </section>
                 </div>
                 </main>
-            </div> */}
+            </div>
 
             <Footer />
         </div>
@@ -89,23 +89,23 @@ type Project = {
   webLink: string
 }
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const { data } = await api.get('/api/findProjects')
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('/api/findProjects')
 
-//   const paths = data.map((project: Project) => ({
-//     params: { id: project.id },
-//   }))
+  const paths = data.map((project: Project) => ({
+    params: { id: project.id },
+  }))
 
-//   return { paths, fallback: false }
-// }
+  return { paths, fallback: false }
+}
 
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const { data } = await api.get(`/api/projects/${params.id}`)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { data } = await api.get(`/api/projects/${params.id}`)
 
-//   const project = data
+  const project = data
 
-//   return {
-//     props: { project },
-//     revalidate: 60 * 60 * 24
-//   }
-// }
+  return {
+    props: { project },
+    revalidate: 60 * 60 * 24
+  }
+}
