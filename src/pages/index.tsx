f94f83429968878d3cfcdd137c313716880bfee7
Marcelino <marceometry@gmail.com>
2021-05-21 15:14:03 -0300

    add meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,10 +20,10 @@ export default function Home({ projects }: HomeProps) {
     <div>
       <Head>
         <title>Marcelino | FrontEnd Dev</title>
-        <meta property="og:title" content="Marcelino | FrontEnd Dev" />
-        <meta property="og:description" content="Site que desenvolvi utilizando React e Next.js para servir como meu portfólio pessoal." />
-        <meta property="og:image" content="/images/wallpaper.png" />
-        <meta property="og:url" content="https://marcelino-frontend.vercel.app/" />
+        <meta property="title" content="Marcelino | FrontEnd Dev" />
+        <meta property="description" content="Site que desenvolvi utilizando React e Next.js para servir como meu portfólio pessoal." />
+        <meta property="image" content="/images/portfolio2.png" />
+        <meta property="url" content="https://marcelino-frontend.vercel.app/" />
       </Head>
   
       <div id="home" className={css.container}>
