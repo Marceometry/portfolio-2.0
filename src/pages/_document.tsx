ad537a9495aacae4f2a67a20d6f303d1afc6e829
Marcelino Teixeira <marceometry@gmail.com>
2021-06-01 18:52:20 -0300

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,13 +9,8 @@ export default class MyDocument extends Document {
 
           <meta name="title" content="Marcelino | FrontEnd Dev" />
           <meta name="description" content="Site que desenvolvi utilizando React e Next.js para servir como meu portfólio pessoal." />
-          <meta name="image" content="https://marcelino-frontend.vercel.app/images/wallpaper.png" />
+          <meta name="image" content="https://marcelino-frontend.vercel.app/images/portfolio2.png" />
           <meta name="url" content="https://marcelino-frontend.vercel.app/" />
-
-          <meta property="og:title" content="Marcelino | FrontEnd Dev" />
-          <meta property="og:description" content="Site que desenvolvi utilizando React e Next.js para servir como meu portfólio pessoal." />
-          <meta property="og:image" content="https://marcelino-frontend.vercel.app/images/wallpaper.png" />
-          <meta property="og:url" content="https://marcelino-frontend.vercel.app/" />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
